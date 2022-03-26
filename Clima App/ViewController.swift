363ccd83f5247bd7b98d5fa7cfca1fe6ad4fae0f
NatachaIrken <victoria1236@gmail.com>
2022-03-26 20:28:51 -0300

    Clima app

--- Clima App/ViewController.swift
+++ Clima App/ViewController.swift
@@ -1,19 +1,52 @@
-//
-//  ViewController.swift
-//  Clima App
-//
-//  Created by Natacha on 26-03-22.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var temperatura: UILabel!
+    
+    @IBOutlet weak var ciudad: UILabel!
+    
+    @IBOutlet weak var buscarTextf: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buscarTextf.delegate = self
+    }
+    
+    
+    @IBAction func botonBuscar(_ sender: Any) {
+        buscarTextf.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        buscarTextf.endEditing(true)
+        print(buscarTextf.text!)
+        return true
+    }
+    
+    
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if buscarTextf.text! == "" {
+            return true
+            
+        }else {
+            textField.placeholder = "Escribe algo"
+            return false
+        }
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        buscarTextf.text! = ""
+        
     }
 
-
+    
 }
-
